cdb8ac40391797420312e66167922834076835f8
RyanAmmarKhodja <164581757+RyanAmmarKhodja@users.noreply.github.com>
2025-07-21 00:09:56 +0100

    pdf generation (contains errors, not ready)

---
+++ controllers/InvoiceDocument .cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QuestPDF.Drawing;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using StockIt_2.models;
+
+
+
+namespace StockIt_2.controllers
+{
+    public class InvoiceDocument : IDocument
+    {
+        public Bon bon { get; }
+
+        public InvoiceDocument(Bon model)
+        {
+            bon = model;
+        }
+
+
+
+        public DocumentMetadata GetMetadata() => DocumentMetadata.Default;
+        public DocumentSettings GetSettings() => DocumentSettings.Default;
+        public void Compose(IDocumentContainer container)
+        {
+            container
+                .Page(page =>
+                {
+                    page.Margin(50);
+
+                    page.Header().Height(100).Background(Colors.Grey.Lighten1);
+                    page.Content().Background(Colors.Grey.Lighten3);
+                    page.Footer().Height(50).Background(Colors.Grey.Lighten1);
+                });
+       
+            container
+                .Page(page =>
+                {
+                    page.Margin(50);
+
+                    page.Header().Element(ComposeHeader);
+                    page.Content().Element(ComposeContent);
+
+                    page.Footer().AlignCenter().Text(x =>
+                    {
+                        x.CurrentPageNumber();
+                        x.Span(" / ");
+                        x.TotalPages();
+                    });
+                });
+        }
+
+        void ComposeHeader(IContainer container)
+        {
+            container.Row(row =>
+            {
+                row.RelativeItem().Column(column =>
+                {
+                    column.Item()
+                        .Text($"Bon de récéption #{bon.Id}")
+                        .FontSize(20).SemiBold().FontColor(Colors.Blue.Medium);
+
+                    column.Item().Text(text =>
+                    {
+                        text.Span("Freha Le: ").SemiBold();
+                        text.Span($"{bon.Date:d}");
+                    });
+
+                    
+                });
+
+                row.ConstantItem(100).Height(50).Placeholder();
+            });
+        }
+
+        void ComposeContent(IContainer container)
+        {
+            container
+                .PaddingVertical(40)
+                .Height(250)
+                .Background(Colors.Grey.Lighten3)
+                .AlignCenter()
+                .AlignMiddle()
+                .Text("Content").FontSize(16);
+            container.PaddingVertical(40).Column(column =>
+            {
+                column.Spacing(5);
+
+                column.Item().Element(ComposeTable);
+
+                //if (!string.IsNullOrWhiteSpace(bon.Comments))
+                //    column.Item().PaddingTop(25).Element(ComposeComments);
+            });
+        }
+
+        void ComposeTable(IContainer container)
+        {
+            container
+                .Height(250)
+                .Background(Colors.Grey.Lighten3)
+                .AlignCenter()
+                .AlignMiddle()
+                .Text("Table").FontSize(16);
+        }
+
+        void ComposeComments(IContainer container)
+        {
+            container.Background(Colors.Grey.Lighten3).Padding(10).Column(column =>
+            {
+                column.Spacing(5);
+                column.Item().Text("Comments").FontSize(14);
+                //column.Item().Text(Model.Comments);
+            });
+        }
+    }
+}
---
+++ models/Bon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockIt_2.models
+{
+    public class Bon
+    {
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public string fournisseur_nom { get; set; }
+        public string fournisseur_prenom { get; set; }
+        public string transporteur_nom { get; set; }
+        public string transporteur_prenom { get; set; }
+        public string designation { get; set; }
+        public string nbr { get; set; }
+        public string poids_kg { get; set; }
+        public string prix_unitaire { get; set; }
+        public string prix_transport_unitaire { get; set; }
+        public string total_amount { get; set; }
+    }
+}
